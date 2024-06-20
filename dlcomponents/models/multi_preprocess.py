93b2d5a3055ecbb3cf5a0e8a81221be7827f04d5
ash3173 <em2ashwinrajesh@gmail.com>
2024-06-20 15:40:53 +0530

    Minor Update

--- dlcomponents/models/multi_preprocess.py
+++ dlcomponents/models/multi_preprocess.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-def df_to_X_y3(df, window_size,sub1,sub2):
+def df_to_X_y3(df, window_size, sub1, sub2):
   df_as_np = df.to_numpy()
   X = []
   y = []
-  for i in range(len(df_as_np)-window_size):
-    row = [r for r in df_as_np[i:i+window_size]]
-    X.append(row)
-    label = [df_as_np[i+window_size][sub1], df_as_np[i+window_size][sub2]]
-    y.append(label)
+  sub1_index = df.columns.get_loc(sub1)
+  sub2_index = df.columns.get_loc(sub2)
+  for i in range(len(df_as_np) - window_size):
+      row = [r for r in df_as_np[i:i + window_size]]
+      X.append(row)
+      label = [df_as_np[i + window_size][sub1_index], df_as_np[i + window_size][sub2_index]]
+      y.append(label)
   return np.array(X), np.array(y)
-
-
