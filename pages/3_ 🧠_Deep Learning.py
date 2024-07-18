fc363f1f5bee52b52f0cc8e99eb9cc0939eb1569
ash3173 <em2ashwinrajesh@gmail.com>
2024-07-18 15:14:39 +0530

    TCN for multivariate.

--- pages/3_ 🧠_Deep Learning.py
+++ pages/3_ 🧠_Deep Learning.py
@@ -8,6 +8,7 @@ from dlcomponents.models.single_gru import single_GRU
 from dlcomponents.models.multi_lstm import multi_LSTM
 from dlcomponents.models.multi_cnn import multi_CNN
 from dlcomponents.models.multi_gru import multi_GRU
+from dlcomponents.models.multi_tcn import multi_TCN
 from dlcomponents.models.multi_cnn_gru import multi_CNN_GRU
 from dlcomponents.models.multi_cnn_lstm import multi_CNN_LSTM
 from dlcomponents.models.ar import ar_CNN_LSTM
@@ -20,7 +21,7 @@ node = st.sidebar.file_uploader("Upload a Node data file", type=["csv"])
 edge = st.sidebar.file_uploader("Upload a Edge data file", type=["csv"])
 
 # Dropdown menu for model selection
-options = ["Select", "CNN", "LSTM", "GRU", "CNN-GRU","CNN-LSTM","AR CNN-LSTM"]
+options = ["Select", "CNN", "LSTM", "GRU","TCN", "CNN-GRU","CNN-LSTM","AR CNN-LSTM"]
 model_option = st.sidebar.selectbox("Choose a model", options)
 
 analysis_type = st.sidebar.selectbox("Choose analysis type", ["Select", "Single Variate", "Multivariate"])
@@ -194,6 +195,35 @@ elif model_option == "GRU" and analysis_type == "Multivariate":
         
         if st.button("Train GRU Model"):
             multi_GRU(selected_node_features_data, forecast_headers)
+elif model_option == "TCN" and analysis_type == "Multivariate":
+    st.write("TCN model selected for multi-variate analysis.")
+    num_nodes = len(node_data["node"].unique())
+    num_features = len(node_data["feature"].unique())
+    node_index = st.slider("Select the node index", 0, num_nodes - 1)
+
+    temp = node_data
+    st.write("Merged data for node", node_index)
+    selected_node_data = get_node_data_from_merged(merged_data=temp, node_index=node_index)
+    st.write(selected_node_data)
+    st.line_chart(selected_node_data)
+
+    feature_headers = selected_node_data.columns.tolist()
+    selected_feature_headers = st.multiselect("Select feature headers for multivariate analysis", feature_headers, default=[feature_headers[0]])
+
+    selected_node_features_data = selected_node_data[selected_feature_headers]
+    st.write(selected_node_features_data)
+
+    forecast_headers = st.multiselect("Select feature headers for forecasting", selected_feature_headers, default=selected_feature_headers[:2])
+
+    if not forecast_headers:
+        st.warning("Please select at least one feature header for forecasting.")
+    else:
+        selected_forecast_features_data = selected_node_features_data[forecast_headers]
+        st.write("Selected features for forecasting:")
+        st.write(selected_forecast_features_data)
+        
+        if st.button("Train TCN Model"):
+            multi_TCN(selected_node_features_data, forecast_headers)
 elif model_option == "CNN-GRU" and analysis_type == "Multivariate":
     st.write("CNN-GRU model selected for multi-variate analysis.")
     num_nodes = len(node_data["node"].unique())
